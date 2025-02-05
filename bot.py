fddc2896dd37ce8e13190159f530305658d09c22
GhollamallahYoussef <youssef.ghollamallah@laplateforme.io>
2025-02-06 00:53:06 +0100

    first commit

--- bot.py
+++ bot.py
@@ -5,6 +5,21 @@ load_dotenv()
 
 print("Lancement du bot...")
 
-client = discord.Client(intents=discord.Intents.all())
-client.run(os.getenv("DISCORD_TOKEN"))
+bot = discord.Client(intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print("Bot connecté")
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.content.lower() == "bonjour" or message.content.lower() == "salut":
+        channel = message.channel
+        author = message.author
+        await channel.send("Salut ! " + author.mention + )
+
+
+
+bot.run(os.getenv("DISCORD_ENV"))
 
